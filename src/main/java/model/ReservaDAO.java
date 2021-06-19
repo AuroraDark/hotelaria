07f40d1ca1f7509f7a4b72a62a43e1ba0670e6be
Andressa Silva <dessa@DESKTOP-6T83F9R>
2021-06-19 20:27:25 -0300

    Fillipe - ReservaDAO att

--- src/main/java/model/ReservaDAO.java
+++ src/main/java/model/ReservaDAO.java
@@ -27,4 +27,66 @@ public class ReservaDAO {
 		}
 	}
 	
+	/** CRUD CREATE **/
+	public void inserirReserva(Reserva reserva) {
+		String createreserva = "insert into reservas(codCli, dataInicio, dataFim) values (?,str_to_date(?,'%Y-%m-%d'),str_to_date(?,'%Y-%m-%d'))";
+		try {
+			// Abrir a conexão com o BD
+			Connection con = conectar();
+			// Preparar a query para execução no BD
+			PreparedStatement pst = con.prepareStatement(createreserva);
+			// Substituir os '?' pelo conteúdo da variável Cliente
+			pst.setString(1, reserva.getCodCli());
+			pst.setString(2, reserva.getDataInicio());
+			pst.setString(3, reserva.getDataFim());
+			
+			// Executar a query (enviar para o BD)
+			pst.executeUpdate();
+			// Encerrar a conexão
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void inserirReservaQuarto(Reserva reserva,Quarto quarto) {
+		String createreservaquarto = "insert into reserva_quartos(numQuarto, dataInicioReserva, dataFimReserva) values (?,str_to_date(?,'%Y-%m-%d'),str_to_date(?,'%Y-%m-%d'))";
+		try {
+			// Abrir a conexão com o BD
+			Connection con = conectar();
+			// Preparar a query para execução no BD
+			PreparedStatement pst = con.prepareStatement(createreservaquarto);
+			// Substituir os '?' pelo conteúdo da variável Cliente
+			pst.setInt(1, quarto.getNumQuarto());
+			pst.setString(2, reserva.getDataInicio());
+			pst.setString(3, reserva.getDataFim());
+			
+			// Executar a query (enviar para o BD)
+			pst.executeUpdate();
+			// Encerrar a conexão
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	public void inserirPagamento(Pagamento pagamento) {
+		String createpagamento = "insert into pagamento(formaPagamento, valorTotal) values (?,?)";
+		try {
+			// Abrir a conexão com o BD
+			Connection con = conectar();
+			// Preparar a query para execução no BD
+			PreparedStatement pst = con.prepareStatement(createpagamento);
+			// Substituir os '?' pelo conteúdo da variável Cliente
+			pst.setString(1, pagamento.getFormaPagamento());
+			pst.setDouble(2, pagamento.getValorTotal());
+			
+			// Executar a query (enviar para o BD)
+			pst.executeUpdate();
+			// Encerrar a conexão
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
